9bbed31e49043bf79682bba24c51ee92e8c87e7e
Chijioke Ndubisi <cjndubisi@gmail.com>
2017-02-25 03:55:47 +0100

    Initial navigation

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,23 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class Wanted extends Component {
+import React, { Component, PropTypes } from 'react';
+import { AppRegistry, NavigatorIOS, Text } from 'react-native';
+import SignupForm from './components/SignupForm'
+export default class WantedApp extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        initialRoute={{
+          title: 'Create Account',
+          component: SignupForm,
+          navigationBarHidden: true
+        }}
+        style={{flex: 1}}
+      />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
-AppRegistry.registerComponent('Wanted', () => Wanted);
+AppRegistry.registerComponent('Wanted', () => WantedApp);
